36c40ab41dda6b07900591d9e2c12116ff7bf14a
kameel4 <74598708+kameel4@users.noreply.github.com>
2024-10-14 19:10:08 +0300

    code refactoring added to main directory

---
+++ after_refactoring_kshaimiev.cpp
@@ -0,0 +1,31 @@
+﻿#include <cmath>
+#include <iostream>
+
+int main()
+{
+    double sequence[10];
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << "Введите элемент под номером: " << i + 1 << std::endl;
+        std::cin >> sequence[i];
+    }
+
+    bool is_increasing = true;
+    for (int i = 1; i < 10; i++)
+    {
+        if (sequence[i]<sequence[i-1]){
+            is_increasing = false;
+            break;
+        }
+    }
+    
+    if (is_increasing) {
+        std::cout << "Последовательность возрастающая" << std::endl;
+    }
+    else{
+        std::cout << "Последовательность не возрастающая" << std::endl;
+    }
+
+    return 0;
+}
+
